8b65d9b6a010012d328a554fe060bf60461037ea
devil1437 <r02922029@csie.ntu.edu.tw>
2014-05-05 20:51:33 +0800

    Add PowerManagerService.java

--- copyFilesToRepo.sh
+++ copyFilesToRepo.sh
@@ -15,6 +15,9 @@ AndroidMkFolder=/home/howard/Nexus4/Nexus4Origin/frameworks/base/
 PowerManager=/home/howard/Nexus4/Nexus4Origin/frameworks/base/core/java/android/os/PowerManager.java
 PowerManagerFolder=/home/howard/Nexus4/Nexus4Origin/frameworks/base/core/java/android/os/
 
+PowerManagerService=/home/howard/Nexus4/Nexus4Origin/frameworks/base/services/java/com/android/server/power/PowerManagerService.java
+PowerManagerServiceFolder=/home/howard/Nexus4/Nexus4Origin/frameworks/base/services/java/com/android/server/power/
+
 if diff $MultiResourceManagerService MultiResourceManagerService.java >/dev/null ; then
 	echo "MultiResourceManagerService.java is same"
 else
@@ -33,19 +36,26 @@ if diff $IMultiResourceManagerService IMultiResourceManagerService.aidl >/dev/nu
 	echo "IMultiResourceManagerService.aidl is same"
 else
 	rm -f $IMultiResourceManagerService
-	cp IMultiResourceManagerService.aidl /home/howard/Nexus4/Nexus4Origin/frameworks/base/core/java/android/os/
+	cp IMultiResourceManagerService.aidl $IMultiResourceManagerServiceFolder
 fi
 
 if diff $AndroidMk Android.mk >/dev/null ; then
 	echo "Android.mk is same"
 else
 	rm -f $AndroidMk
-	cp Android.mk /home/howard/Nexus4/Nexus4Origin/frameworks/base/
+	cp Android.mk $AndroidMkFolder
 fi
 
 if diff $PowerManager PowerManager.java >/dev/null ; then
 	echo "PowerManager.java is same"
 else
 	rm -f $PowerManager
-	cp PowerManager.java /home/howard/Nexus4/Nexus4Origin/frameworks/base/core/java/android/os/
+	cp PowerManager.java $PowerManagerFolder
+fi
+
+if diff $PowerManagerService PowerManagerService.java >/dev/null ; then
+	echo "PowerManagerService.java is same"
+else
+	rm -f $PowerManagerService
+	cp PowerManagerService.java $PowerManagerServiceFolder
 fi
